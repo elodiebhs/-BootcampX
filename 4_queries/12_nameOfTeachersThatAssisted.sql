39b940a7061f097889459a856d19f64de9fc874a
elodiebhs <elodiebouthors@hotmail.com>
2021-03-02 23:50:35 +0000

    name of all teachers that performed an assistance request during a cohort and adding num of assistances

---
+++ 4_queries/12_nameOfTeachersThatAssisted.sql
@@ -0,0 +1,8 @@
+---Get the name of all teachers that performed an assistance request during a cohort
+SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
+FROM assistance_requests
+JOIN teachers ON assistance_requests.teacher_id = teachers.id
+JOIN students ON assistance_requests.student_id = students.id
+JOIN cohorts ON cohort_id = cohorts.id
+WHERE cohorts.name = 'JUL02'
+ORDER BY teacher;
